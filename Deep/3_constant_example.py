20471e55c181bbd724397e0f616b990413ea3b59
Zilch <hezl1592@163.com>
2019-02-20 17:13:58 +0800

    constant

--- Deep/3_constant_example.py
+++ Deep/3_constant_example.py
@@ -6,7 +6,7 @@ with tf.Graph().as_default():
     sess = tf.InteractiveSession()
 
     # 定义常量：不可更改的张量
-    a = tf.constant(value=[1.0, 2.0], shape=[3, 2])
+    a = tf.constant(value=[1.0, 2.0], shape=[3, 2], name='1')
     sess.run(a)
     print(sess.run(a))
     print(a)
